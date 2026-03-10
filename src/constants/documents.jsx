bd123dfbf6a1ca3f1563bec578764cb1910df0c6
Merouane Meddad <merouane.meddad@sofinance.dz>
2026-03-10 13:48:14 +0100

    added all declarations in constents.js

--- src/constants/documents.jsx
+++ src/constants/documents.jsx
@@ -2,12 +2,14 @@ export const documentTypes = [
   { abbr: 'DEE', fullName: ' Déclaration des Engagements Extérieurs par signature' , frequency:"Mensuelle" },
   { abbr: 'DGR', fullName: 'Déclaration des Grands Risques', frequency:"Trimestrielle" },
   { abbr: 'DPC', fullName: 'Déclaration Position de Change', frequency:"Mensuelle" },
-  { abbr:'DCS' ,fullName:'Déclaration des coefficients de solvabilité', frequency:"Trimestrielle"}
-  //{ abbr: 'RRO', fullName: 'Régime des Réserves Obligatoires' },
-  //{ abbr: 'OAD', fullName: " Ouverture et Apurement des Dossiers de Domiciliation à l'Importation et à l'Exportation des Biens et Services" },
-  //{ abbr: 'ACE ', fullName: 'Avoirs en devises des banques chez les Correspondants Etrangers' },
-  //{ abbr: 'BOP ', fullName: ' Déclaration de la Balance de Paiement' },
-
-
+  { abbr:'DCS' ,fullName:'Déclaration des coefficients de solvabilité', frequency:"Trimestrielle"},
+  { abbr: 'CFP', fullName: 'Calcul du Coefficient de Fonds Propres et de Ressources Permanentes' , frequency:"Annuelle"},
+  { abbr: 'ECP', fullName: 'Etat des déclarations des crédits consentis par les banques et établissements financiers aux entreprises dont ils détiennent une participation au capital' , frequency:"Bimensuelle"},
+  { abbr: 'TEG', fullName: " Taux effectif global moyen" , frequency:"Semestrielle"},
+  { abbr: 'CBR', fullName: "Etat des Cinquantes (50) plus Gros Clients ayant Bénéficié d’un Rééchelonnement", frequency:"Semestrielle" },
+  { abbr: 'CPD', fullName: 'Liste des Cinquante (50) Plus Gros Clients Douteux du Secteur Prive', frequency:"Semestrielle" },
+  { abbr: 'PEI', fullName: ' Déclaration des prêts et emprunts interbancaires', frequency:"Semestrielle" },
+  { abbr: 'RCC', fullName: ' Répartition des créances et engagements classés' , frequency:"Trimestrielle"},
+  { abbr: 'SFE', fullName: " Déclaration des crédits rééchelonnés au titre du soutien financier de l'Etat", frequency:"Mensuelle" },
 
 ];
